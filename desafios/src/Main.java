d4267e3ff655796d6796640ef88137ec1f669c41
Zorke <ferrari.work@gmail.com>
2026-01-08 14:36:04 -0300

    feat: add user input validation for factorial range 1-12

--- desafios/src/Main.java
+++ desafios/src/Main.java
@@ -1,9 +1,29 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Operation op = new Factorial();
-        int number = 0;
-        System.out.println("Fatorial de " + number + " é: " + op.calculate(number));
+        Scanner scanner = new Scanner(System.in);
+        int number;
+
+        while (true) {
+            System.out.print("Enter a number between 1 and 12 to calculate its factorial: ");
+            if (scanner.hasNextInt()) {
+                number = scanner.nextInt();
+                if (number >= 1 && number <= 12) {
+                    break;
+                } else {
+                    System.out.println("Please enter a valid number between 1 and 12.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter an integer.");
+                scanner.next(); // Consume invalid input
+            }
+        }
+        
+        System.out.println("Factorial of " + number + " is: " + op.calculate(number));
+        scanner.close();
     }
 }
