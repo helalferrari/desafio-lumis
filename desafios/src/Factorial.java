6c47f4f8f92fb63ec0b27f21e82aa50ace1aed9a
Zorke <ferrari.work@gmail.com>
2026-01-08 14:33:06 -0300

    feat: implement recursive factorial with Operation interface

---
+++ desafios/src/Factorial.java
@@ -0,0 +1,9 @@
+public class Factorial implements Operation {
+    @Override
+    public long calculate(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * calculate(n - 1);
+    }
+}
